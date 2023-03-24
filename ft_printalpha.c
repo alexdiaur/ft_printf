2af58f060f5361c3760c2681986b2c73e054bb37
Alejandra Diaz <fdiaz@student.42madrid.com>
2023-03-24 20:44:36 +0100

    Add ft_printalpha functions

--- ft_printalpha.c
+++ ft_printalpha.c
@@ -3,11 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printalpha.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fdiaz <marvin@42.fr>                       +#+  +:+       +#+        */
+/*   By: fdiaz <fdiaz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 13:46:21 by fdiaz             #+#    #+#             */
-/*   Updated: 2023/03/20 13:47:33 by fdiaz            ###   ########.fr       */
+/*   Updated: 2023/03/24 20:44:09 by fdiaz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_printf.h"
 
+int	ft_putchar(char c)
+{
+	write(1, &c, 1);
+	return (c);
+}
+
+int	ft_putstring(char *c)
+{
+	int	len;
+
+	if (!c)
+		return (ft_putstring("(null)"));
+	len = 0;
+	while (c[len])
+		return (ft_putchar(c[len]));
+	return (len);
+}
